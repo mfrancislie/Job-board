e56d2fce7ccabd8e430615094a1bec2866ecef74
Francislie <mfrancislie@gmail.com>
2024-04-05 02:09:24 -0700

    Authentication: logging-out

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,8 +11,39 @@
  mx-auto mt-10 max-w-3xl bg-gradient-to-r 
  from-indigo-100 via-sky-100 to-emerald-100 text-slate-700">
 
- {{-- if the not yet login gues will display --}}
- {{ auth()->user()->name ?? 'Guest' }}
+ {{-- 
+
+  if the not yet login gues will display 
+  {{ auth()->user()->name ?? 'Guest' }}
+
+  --}}
+
+  <nav class="mb-8 flex justify-between text-lg font-medium">
+    <ul class="flex space-x-2">
+      <li>
+        <a href="{{ route('jobs.index') }}">Home</a>
+      </li>
+    </ul>
+
+    <ul class="flex space-x-2">
+      @auth
+        <li>
+          {{ auth()->user()->name ?? 'Anynomus' }}
+        </li>
+        <li>
+          <form action="{{ route('auth.destroy') }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button>Logout</button>
+          </form>
+        </li>
+      @else
+        <li>
+          <a href="{{ route('auth.create') }}">Sign in</a>
+        </li>
+      @endauth
+    </ul>
+  </nav>
 
 
  
