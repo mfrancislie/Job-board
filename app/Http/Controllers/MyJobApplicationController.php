b12d636d20143b10f96648e3e1d7c77cf10831d0
Francislie <mfrancislie@gmail.com>
2024-04-18 18:13:07 -0700

    My Applications: Controller & view

---
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyJobApplicationController extends Controller
+{
+
+    public function index()
+    {
+        
+    
+        return view(
+            'my_job_application.index',
+            [
+                'applications' => auth()->user()->jobApplications()
+                ->with(['job', 'job.employer']) // Eager load job and employer details
+                ->latest()
+                ->get()
+            ]
+        );
+    }
+    
+
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
