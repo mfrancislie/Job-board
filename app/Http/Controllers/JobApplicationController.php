61a6f131bee9c3a4e66b30bf38a483b20a31e446
Francislie <mfrancislie@gmail.com>
2024-04-18 23:23:09 -0700

    My Applications: Uploading File

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -24,12 +24,29 @@ class JobApplicationController extends Controller
     public function store(Job $job, Request $request)
     {
         Gate::authorize('apply', $job);
+
+     $validatedData = $request->validate([
+            'expected_salary' => 'required|min:1|max:1000000',
+            'cv' => 'required|file|mimes:pdf|max:2048'
+     ]);
+
+     $file = $request->file('cv');
+     $path = $file->store('cvs', 'private');
+     
         $job->jobApplications()->create([
             'user_id' => $request->user()->id,
+            'expected_salary' => $validatedData['expected_salary'],
+            'cv_path' => $path
+
+            /* 
+            before
+
             ...$request->validate([
                 'expected_salary' => 'required|min:1|max:1000000'
-            ])
+
+           */
         ]);
+
 
         //this will be redirect to jobs show after apply
         return redirect()->route('jobs.show', $job)
